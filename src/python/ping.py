8534fc43f464d9465ad64d2089b17fe806df9c66
Lucas Jandrew <lucas@jandrew.com>
2013-08-13 23:41:26 -0700

    Prints out a timestamp

--- src/python/ping.py
+++ src/python/ping.py
@@ -3,11 +3,11 @@ import time
 
 class Ping:
 	def __init__(self, ping):
-		match = re.search('time=(\d+) ', ping)
-		self.time = int(match.group(1))
+		match = re.search('time=([\d\.]+) ', ping)
+		self.time = float(match.group(1))
 
 	def sleep(self):
 		if self.time >= 1000:
 			return
 
-		time.sleep(1 - (self.time/1000.0))
+		time.sleep(1 - (self.time/1000))
--- src/python/pongsong.py
+++ src/python/pongsong.py
@@ -5,6 +5,7 @@ from ping import Ping
 delay = 1000
 
 while True:
-	ping = Ping(subprocess.check_output(['ping', 'fapiko.com', '-c 1']))
-	print ping.time
+	ping = Ping(subprocess.check_output(['ping', 'minecraft.fapiko.com', '-c 1']))
+	print "%s %s" % (time.strftime('%a, %d %b %Y %H:%M:%S +0000', time.gmtime()), ping.time)
+	# print ping.time
 	ping.sleep()
