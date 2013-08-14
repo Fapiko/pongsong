4cb00ec69c2f8df0535f1e036009708261f9be2b
Lucas Jandrew <lucas@jandrew.com>
2013-08-14 16:29:14 -0700

    Added a case for ping failure

--- src/python/pongsong.py
+++ src/python/pongsong.py
@@ -2,10 +2,12 @@ import subprocess
 import time
 from ping import Ping
 
-delay = 1000
+hostname = 'minecraft.fapiko.com'
 
 while True:
-	ping = Ping(subprocess.check_output(['ping', 'minecraft.fapiko.com', '-c 1']))
-	print "%s %s" % (time.strftime('%a, %d %b %Y %H:%M:%S +0000', time.gmtime()), ping.time)
-	# print ping.time
-	ping.sleep()
+	try:
+		ping = Ping(subprocess.check_output(['ping', hostname, '-c 1', '-w 5']))
+		print "%s %s %s" % (time.strftime('%a, %d %b %Y %H:%M:%S +0000', time.gmtime()), hostname, ping.time)
+		ping.sleep()
+	except subprocess.CalledProcessError:
+		print "%s %s %s" % (time.strftime('%a, %d %b %Y %H:%M:%S +0000', time.gmtime()), hostname, "Timeout")
